c086ab4d617401d1c75392e41867f228915e3021
Yevgeniy <OstapenkoYevgeniy@gmail.com>
2016-03-17 14:31:04 +0600

    Alpha 0.1

--- src/main/java/com/gmail/ostapenkoyevgeniy/skynet/support/AppConstant.java
+++
@@ -1,8 +0,0 @@
-package com.gmail.ostapenkoyevgeniy.skynet.support;
-
-public class AppConstant {
-    public static final String LOG_TAG_DEBUG = "appDebug";
-    public static final String LOG_TAG_ERROR = "appError";
-
-    private AppConstant() {}
-}
